b82ae2969badba0441e4eea980fb30e4de169073
OmicronPersei <wheeler.weise@gmail.com>
2017-08-10 18:24:27 -0500

    Added more to the test.

--- src/NUnitFramework/tests/Internal/Filters/MultipleFilterTests.cs
+++ src/NUnitFramework/tests/Internal/Filters/MultipleFilterTests.cs
@@ -9,6 +9,7 @@
             var filter = new OrFilter(new NotFilter(new CategoryFilter("Dummy")), new CategoryFilter("Dummy"));
 
             Assert.False(filter.IsExplicitMatch(_explicitFixture));
+            Assert.False(filter.Pass(_explicitFixture));
         }
     }
 }
