a69c9504fbbb1e57d09fe96273fc82fa71af7046
Wheeler Weise <wheeler.weise@gmail.com>
2017-08-27 11:30:48 -0500

    Refactored and added copyright preamble.

--- src/NUnitFramework/framework/Internal/TypeNameDifference.cs
+++ src/NUnitFramework/framework/Internal/TypeNameDifference.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿// ***********************************************************************
+// Copyright (c) 2017 Charlie Poole, Rob Prouse
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+using System;
 
 namespace NUnit.Framework.Internal
 {
     /// <summary>
-    /// Used for generating the type difference between objects.
+    /// Used for resolving the type difference between objects.
     /// </summary>
     public class TypeNameDifference
     {
@@ -16,27 +39,28 @@ namespace NUnit.Framework.Internal
         /// <param name="actualType">Output of the unique type name for actual</param>
         public void ResolveTypeNameDifference(object expected, object actual, out string expectedType, out string actualType)
         {
-            ResolveSingularTypeNameDifference(expected, actual, out expectedType, out actualType);
+            ShortenDifferingTypeNames(out expectedType, out actualType, expected.GetType().ToString(), actual.GetType().ToString());
         }
 
-        private static void ResolveSingularTypeNameDifference(object expected, object actual, out string expectedType, out string actualType)
+        private static void ShortenDifferingTypeNames(out string expectedTypeShortened, out string actualTypeShortened, string expectedOriginalType, string actualOriginalType)
         {
-            string[] expectedOriginalType = expected.GetType().ToString().Split('.');
-            string[] actualOriginalType = actual.GetType().ToString().Split('.');
+            string[] expectedOriginalTypeSplit = expectedOriginalType.Split('.');
+            string[] actualOriginalTypeSplit = actualOriginalType.Split('.');
+
             int actualStart = 0, expectStart = 0;
-            for (int expectLen = expectedOriginalType.Length - 1, actualLen = actualOriginalType.Length - 1;
+            for (int expectLen = expectedOriginalTypeSplit.Length - 1, actualLen = actualOriginalTypeSplit.Length - 1;
                 expectLen >= 0 && actualLen >= 0;
                 expectLen--, actualLen--)
             {
-                if (expectedOriginalType[expectLen] != actualOriginalType[actualLen])
+                if (expectedOriginalTypeSplit[expectLen] != actualOriginalTypeSplit[actualLen])
                 {
                     actualStart = actualLen;
                     expectStart = expectLen;
                     break;
                 }
             }
-            expectedType = String.Join(".", expectedOriginalType, expectStart, expectedOriginalType.Length - expectStart);
-            actualType = String.Join(".", actualOriginalType, actualStart, actualOriginalType.Length - actualStart);
+            expectedTypeShortened = String.Join(".", expectedOriginalTypeSplit, expectStart, expectedOriginalTypeSplit.Length - expectStart);
+            actualTypeShortened = String.Join(".", actualOriginalTypeSplit, actualStart, actualOriginalTypeSplit.Length - actualStart);
         }
     }
 }
